98521ec6ca49a773a1d381fe4211550636d5e915
xavism <xavi.sanchez.mir@gmail.com>
2019-11-24 12:53:34 +0100

    red-phase

--- tests/unit/SpeakersView.spec.js
+++ tests/unit/SpeakersView.spec.js
@@ -1,9 +1,26 @@
 import { shallowMount } from '@vue/test-utils'
 import SpeakersView from '@/views/SpeakersView'
+import SearchForm from '@/components/SearchForm'
+import SpeakersList from '@/components/SpeakersList'
+
 
 describe('SpeakersView', () => {
   it('it renders the component', () => {
+    // arrange
     const wrapper = shallowMount(SpeakersView)
+
+    // assert
     expect(wrapper.html()).toMatchSnapshot()
+  })
+
+  it('it renders the right', () => {
+    // arrange
+    const wrapper = shallowMount(SpeakersView)
+    const searchForm = wrapper.find(SearchForm)
+    const speakersList = wrapper.find(SpeakersList)
+
+    // assert
+    expect(searchForm.exists()).toBe(true)
+    expect(speakersList.exists()).toBe(true)
   })
 })
