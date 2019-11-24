584d1c592c2aa458a8f354550331d2477f400443
xavism <xavi.sanchez.mir@gmail.com>
2019-11-24 22:00:01 +0100

    Added action when clicking

--- src/components/SearchForm.vue
+++ src/components/SearchForm.vue
@@ -1,16 +1,23 @@
-<template>
+  <template>
   <div>
     <input type="text" v-model="inputData">
-    <button></button>
+    <button @click="fetchSpeaker(inputData) "></button>
   </div>
 </template>
 
 <script>
+import { mapActions } from 'vuex'
+
 export default {
   data() {
     return {
       inputData: ''
     }
+  },
+  methods: {
+    ...mapActions({
+      fetchSpeaker: 'FETCH_SPEAKER'
+    })
   }
 }
 </script>
