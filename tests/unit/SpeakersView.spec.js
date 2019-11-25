c9773f4cb9e023a03840709732e6691a61e1aa9d
xavism <xavi.sanchez.mir@gmail.com>
2019-11-25 02:12:19 +0100

    red phase

--- tests/unit/SpeakersView.spec.js
+++ tests/unit/SpeakersView.spec.js
@@ -2,7 +2,7 @@ import { shallowMount } from '@vue/test-utils'
 import SpeakersView from '@/views/SpeakersView'
 
 describe('SpeakersView', () => {
-  it('it renders the component', () => {
+  it('renders the component', () => {
     const wrapper = shallowMount(SpeakersView)
     expect(wrapper.html()).toMatchSnapshot()
   })
