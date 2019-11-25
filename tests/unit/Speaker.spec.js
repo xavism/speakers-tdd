ba783f089e31e9d3c0475a25dba3d81b03a09237
xavism <xavi.sanchez.mir@gmail.com>
2019-11-25 02:07:03 +0100

    red phase

---
+++ tests/unit/Speaker.spec.js
@@ -0,0 +1,42 @@
+import { shallowMount } from '@vue/test-utils'
+import Speaker from '@/components/Speaker'
+
+describe('Speaker', () => {
+  let props
+  let speaker = {
+    username: '@myname',
+    name: 'My Name',
+    image:'https://myimage.com'
+  }
+  beforeEach(() => {
+    props = {
+      speaker
+    }
+  })
+
+  const build = () => {
+    const wrapper = shallowMount(Speaker, {
+      // Add props while mounting
+      propsData: props
+    })
+
+    return {
+      wrapper
+    }
+  }
+
+  it('renders the component', () => {
+    // arrange
+    const { wrapper } = build()
+    // assert
+    expect(wrapper.html()).toMatchSnapshot()
+  })
+
+  it('renders the right', () => {
+    // arrange
+    const { wrapper } = build()
+    // assert
+    expect(wrapper.text()).toContain(speaker.username)
+    expect(wrapper.find('a').attributes('href')).toBe(speaker.image)
+  })
+})
