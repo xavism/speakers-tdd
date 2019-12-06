3f673b191d78d26867fc5a2b92a4ed8c4fd8eb47
xavism <xavi.sanchez.mir@gmail.com>
2019-12-06 12:50:45 +0100

    actions finished

--- src/store/actions.js
+++ src/store/actions.js
@@ -1,1 +1,15 @@
-export default {};
+import api from '@/api';
+
+export default {
+  FETCH_SPEAKER({ commit }, { username }) {
+    return new Promise(async (resolve, reject) => {
+      try {
+        const user = await api.searchSpeaker(username);
+        commit('SET_SPEAKER', user);
+        resolve(user);
+      } catch (error) {
+        reject(error);
+      }
+    });
+  },
+};
