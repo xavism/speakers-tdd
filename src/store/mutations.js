d9849fbf977f2c3099eb5c20823dcf990ad44ef2
xavism <xavi.sanchez.mir@gmail.com>
2019-12-06 13:37:58 +0100

    Adding implementation of mutation

--- src/store/mutations.js
+++ src/store/mutations.js
@@ -1,5 +1,11 @@
 export default {
   SET_SPEAKER(state, speaker) {
-    state.speakers.push(speaker);
+    if (!state.speakers.find(s => s.username === speaker.login)) {
+      state.speakers.push({
+        username: speaker.login,
+        name: speaker.name,
+        image: speaker.avatar_url
+      });
+    }
   },
 };
