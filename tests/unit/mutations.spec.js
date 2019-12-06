209773eaa6ac6e40c04be27576b1ecf8e44df8ba
xavism <xavi.sanchez.mir@gmail.com>
2019-12-06 13:10:26 +0100

    second test red phase

--- tests/unit/mutations.spec.js
+++ tests/unit/mutations.spec.js
@@ -20,4 +20,15 @@ describe('Mutations', () => {
     expect(state.speakers.length).toBe(1);
     expect(state.speakers[0]).toEqual(expectedSpeaker)
   })
+
+  it('does not allow repetitions', () => {
+    // arrange
+    const expectedSpeaker = speaker
+    state = [expectedSpeaker]
+    // act
+    mutations.SET_SPEAKER(state, expectedSpeaker)
+
+    // assert
+    expect(state.speakers.length).toBe(1);
+  })
 })
